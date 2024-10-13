4edf9406153550e1de8fe49773ed35e494abedaa
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-10-13 22:11:00 +0200

    Create config_woo.sh

---
+++ shell/config_woo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wp plugin install woocommerce woocommerce-pdf-invoices-packing-slips --activate
+
+wp plugin install side-cart-woocommerce woocommerce-table-rate-shipping smntcs-woocommerce-quantity-buttons
