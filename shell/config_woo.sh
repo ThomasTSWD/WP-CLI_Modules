dd6f201c4fde038c2dc48008ac1fe782534805f9
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-11-19 16:46:19 +0100

    Update config_woo.sh

--- shell/config_woo.sh
+++ shell/config_woo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wp plugin install woocommerce woocommerce-pdf-invoices-packing-slips --activate
+wp plugin install woocommerce woocommerce-pdf-invoices-packing-slips wc-hide-shipping-methods --activate
 
 wp plugin install side-cart-woocommerce smntcs-woocommerce-quantity-buttons
