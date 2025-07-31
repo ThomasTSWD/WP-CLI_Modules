888940de6655c19a0f8645f7e0904af0875d8756
Tom TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2025-07-31 10:03:30 +0200

    Update config_wp.sh

--- shell/config_wp.sh
+++ shell/config_wp.sh
@@ -11,9 +11,11 @@ wp theme install https://github.com/ThomasTSWD/WPCLI/raw/main/Divi_child.zip
 
 wp plugin delete akismet hello
 
-wp plugin install https://github.com/ThomasTSWD/tswd-front-end/archive/refs/heads/main.zip simple-divi-shortcode wp-custom-body-class better-search-replace enable-media-replace disable-comments bulk-page-creator ewww-image-optimizer --activate
+wp plugin install simple-divi-shortcode wp-custom-body-class better-search-replace enable-media-replace disable-comments bulk-page-creator ewww-image-optimizer --activate
 
-wp plugin install really-simple-ssl complianz-gdpr wordpress-seo wp-fastest-cache wow-carousel-for-divi-lite popups-for-divi contact-form-7 favicon-by-realfavicongenerator wp-pagenavi autoptimize wp-mail-smtp
+wp plugin install https://github.com/ThomasTSWD/tswd-front-end/archive/refs/heads/main.zip --activate
+
+wp plugin install really-simple-ssl complianz-gdpr wordpress-seo wp-fastest-cache wow-carousel-for-divi-lite popups-for-divi contact-form-7 favicon-by-realfavicongenerator wp-pagenavi autoptimize wp-mail-smtp ewww-image-optimizer
 
 wp comment delete --force $(wp comment list --format=ids)
 
